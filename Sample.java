ec75bbd70da22ae52476e326025681b6b2a6854a
golenius <45516274+golenius@users.noreply.github.com>
2018-12-01 16:42:35 +0100

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,31 @@
+import java.text.DecimalFormat;
+
+public class Sample {
+    int c;
+    public static void main(String[] args) {
+        int a;
+        a = 2;
+        int b = 3;
+        //int x, y, z;
+        String name0 = "Tomek", lastname = "Kowalski";
+        String name = "Wojtek";
+
+        System.out.println(name);
+
+
+        Sample sample = new Sample();
+        System.out.println(sample.c);
+        double v=4.555;
+        //DecimalFormat vv = new DecimalFormat(pattern: "#.####");
+        //DecimalFormat vv = new DecimalFormat();
+        //System.out.println(vv.format(x));
+
+        int x =5;
+        x+=5;
+        int y = 3;
+        double z = 3;
+        System.out.println(10/ (double)3);
+        System.out.println(10/3.);
+    }
+
+}
